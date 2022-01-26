16aff0862371bf2d5bdfaf3e11d340a05b5e0b1c
minh14496 <minh14496@gmail.com>
2022-01-25 21:17:23 -0800

    edit setting

--- speech_gpt3/speech/views.py
+++ speech_gpt3/speech/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def say_hello(request):
+def main(request):
     # return HttpResponse('Hello World!')
     
     # render using html
--- speech_gpt3/speech_gpt3/settings.py
+++ speech_gpt3/speech_gpt3/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'speech'
+    'speech.apps.SpeechConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
